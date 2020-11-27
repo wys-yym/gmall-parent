6a8d3bd33e42a33bdf0a8045d33ec34eea5a1aaf
wys <wys@qq.com>
2020-11-27 20:15:49 +0800

    测试

---
+++ service/service-product/src/main/java/com/atguigu/gmall/product/ServiceProductApplication.java
@@ -0,0 +1,19 @@
+package com.atguigu.gmall.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ProjectName: gmall-parent
+ * @Package: com.atguigu.gmall.product
+ * @Author: WangYongShuai
+ * @Description:
+ * @Date: 2020/11/27 19:34
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class ServiceProductApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceProductApplication.class,args);
+    }
+}
