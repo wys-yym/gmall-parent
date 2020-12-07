640064f5b29283bc2e0a3cda033383fa9f8b1d15
wys <wys@qq.com>
2020-12-07 14:43:12 +0800

    将getItem修改成多线程的方式

--- service/service-item/src/main/java/com/atguigu/gmall/item/service/impl/ItemServiceImpl.java
+++ service/service-item/src/main/java/com/atguigu/gmall/item/service/impl/ItemServiceImpl.java
@@ -1,6 +1,7 @@
 package com.atguigu.gmall.item.service.impl;
 
 import com.alibaba.fastjson.JSON;
+import com.atguigu.gmall.config.ThreadPoolConfig;
 import com.atguigu.gmall.item.service.ItemService;
 import com.atguigu.gmall.model.entity.product.BaseCategoryView;
 import com.atguigu.gmall.model.entity.product.SkuImage;
@@ -14,6 +15,10 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 /**
  * @ProjectName: gmall-parent
@@ -28,20 +33,58 @@ public class ItemServiceImpl implements ItemService {
     @Autowired
     ProductFeignClient productFeignClient;
 
+    @Autowired
+    ThreadPoolExecutor threadPoolExecutor;
+
     @Override
     public Map<String, Object> getItem(Long skuId) {
+        HashMap<String, Object> resultMap = getItemByMultiThread(skuId);
+        return resultMap;
+    }
+
+    private HashMap<String, Object> getItemByMultiThread(Long skuId) {
         HashMap<String, Object> resultMap = new HashMap<>();
-        BigDecimal price = productFeignClient.getPrice(skuId);
-        SkuInfo skuInfo = productFeignClient.getSkuInfo(skuId);
-        List<SpuSaleAttr> spuSaleAttrList = productFeignClient.getSpuSaleAttrList(skuInfo.getSpuId(),skuId);
-        BaseCategoryView categoryView = productFeignClient.getCategoryView(skuInfo.getCategory3Id());
-        Map<String,Long> jsonMap = productFeignClient.getSaleAttrValuesBySpu(skuInfo.getSpuId());
-        resultMap.put("price",price);
-        resultMap.put("skuInfo",skuInfo);
-        resultMap.put("spuSaleAttrList",spuSaleAttrList);
-        resultMap.put("categoryView",categoryView);
-        String json = JSON.toJSONString(jsonMap);
-        resultMap.put("valuesSkuJson",json);
+        CompletableFuture<SkuInfo> skuInfoCompletableFuture = CompletableFuture.supplyAsync(new Supplier<SkuInfo>() {
+            @Override
+            public SkuInfo get() {
+                SkuInfo skuInfo = productFeignClient.getSkuInfo(skuId);
+                resultMap.put("skuInfo", skuInfo);
+                return skuInfo;
+            }
+        }, threadPoolExecutor);
+        CompletableFuture<Void> priceCompletableFuture = CompletableFuture.runAsync(new Runnable() {
+            @Override
+            public void run() {
+                BigDecimal price = productFeignClient.getPrice(skuId);
+                resultMap.put("price", price);
+            }
+        }, threadPoolExecutor);
+        CompletableFuture<Void> spuSaleAttrsCompletableFuture = skuInfoCompletableFuture.thenAcceptAsync(new Consumer<SkuInfo>() {
+            @Override
+            public void accept(SkuInfo skuInfo) {
+                List<SpuSaleAttr> spuSaleAttrList = productFeignClient.getSpuSaleAttrList(skuInfo.getSpuId(), skuId);
+                resultMap.put("spuSaleAttrList", spuSaleAttrList);
+                resultMap.put("spuSaleAttrList", spuSaleAttrList);
+            }
+        }, threadPoolExecutor);
+        CompletableFuture<Void> categoryViewCompletableFuture = skuInfoCompletableFuture.thenAcceptAsync(new Consumer<SkuInfo>() {
+            @Override
+            public void accept(SkuInfo skuInfo) {
+                BaseCategoryView categoryView = productFeignClient.getCategoryView(skuInfo.getCategory3Id());
+                resultMap.put("categoryView", categoryView);
+            }
+        }, threadPoolExecutor);
+        CompletableFuture<Void> valuesSkuJsonCompletableFuture = skuInfoCompletableFuture.thenAcceptAsync(new Consumer<SkuInfo>() {
+            @Override
+            public void accept(SkuInfo skuInfo) {
+                Map<String, Long> jsonMap = productFeignClient.getSaleAttrValuesBySpu(skuInfo.getSpuId());
+                String json = JSON.toJSONString(jsonMap);
+                resultMap.put("valuesSkuJson", json);
+            }
+        }, threadPoolExecutor);
+        //组合线程,等待所有线程执行结束后才执行主线程
+        CompletableFuture.allOf(skuInfoCompletableFuture, priceCompletableFuture, spuSaleAttrsCompletableFuture,
+                categoryViewCompletableFuture, valuesSkuJsonCompletableFuture).join();
         return resultMap;
     }
 }
